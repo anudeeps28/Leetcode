2dfdf03970b952d7d66663ff70729eae84e98668
anudeeps28 <anudeepsharma28@gmail.com>
2023-01-17 14:33:20 -0500

    easy 5

---
+++ removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+
+        int i = 0;
+        for(int j = 1; j < nums.size(); j++){
+           if(nums[i] != nums[j])
+               i++;
+               nums[i] = nums[j];
+           }
+    return i+1;
+    }
+};
+
+int main() {
+    Solution S;
+
+    vector<int> numbers = {1,1,2,3,3,4};
+
+    cout << S.removeDuplicates(numbers);
+    return 0;
+}
