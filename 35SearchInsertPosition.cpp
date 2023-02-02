7d7cd5a1eac980794109793ca301bb75dd5863fc
anudeeps28 <anudeepsharma28@gmail.com>
2023-02-02 11:42:46 -0500

    O(log n)

--- 35SearchInsertPosition.cpp
+++ 35SearchInsertPosition.cpp
@@ -7,17 +7,8 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int length = nums.size();
-
-        auto it = find(nums.begin(), nums.end(), target);
-        if (it == nums.end()) {
-            int j = 0;
-            while(target > nums[j]) {
-                j++;
-            }
-            return j;
-        } else {
-            return it - nums.begin();
-        }
+        auto it = lower_bound(nums.begin(), nums.end(), target);
+        return it - nums.begin();
 
        
     }
