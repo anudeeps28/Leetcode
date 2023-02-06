d265c25037f874f340e23d11d535a75493b0fea9
anudeeps28 <anudeepsharma28@gmail.com>
2023-02-06 12:54:42 -0500

    DP - Bottom Up / Tabulation

--- 70ClimbingStairs.cpp
+++ 70ClimbingStairs.cpp
@@ -4,22 +4,12 @@ using namespace std;
 
 class Solution {
 public:
-    int helperFunction(vector<int> &dp, int n) {
-        if (n < 0) return 0;
-        if (n == 1 || n == 0) return 1;
-
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-
-        return dp[n] = helperFunction(dp, n-1) + helperFunction(dp, n-2);
-    }
-
     int climbStairs(int n) {
-        
-        vector<int> dp(n+1, -1); // initializing (n+1) elements with -1
-        return helperFunction(dp, n);
-
+        vector<int> dp(n+1);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++)   
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
 
