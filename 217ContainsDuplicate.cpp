6e53929961e171f185c3e8fad1b905f8404a0b21
anudeeps28 <anudeepsharma28@gmail.com>
2023-02-19 15:50:45 -0500

    O(n)

--- 217ContainsDuplicate.cpp
+++ 217ContainsDuplicate.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 #include<vector>
+#include<unordered_set>
 using namespace std;
 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        unordered_set<int> s;
+        
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == nums[i+1]){
+            if (s.find(nums[i]) != s.end()) {
                 return true;
             }
+            s.insert(nums[i]);
         }
+        
         return false;
     }
 };
