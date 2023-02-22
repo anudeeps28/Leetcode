3589eaf10f08aa9de61234c3c4e0b2c0e38dcbc1
anudeeps28 <anudeepsharma28@gmail.com>
2023-02-22 10:41:25 -0500

    2sum-sorted

---
+++ 15_3Sum.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        
+        int n = nums.size();
+        if (n < 3) {
+            return result;
+        }
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] > 0) { // no triplet (sum = 0) if first element is positive
+                break;
+            }
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = n - 1;
+            
+            // the main logic!
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    j++;
+                    
+                    while (j < k && nums[k - 1] == nums[k]) {
+                        k--;
+                    }
+                    k--;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
+int main () {
+    Solution s;
+    vector<int> v = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> answer = s.threeSum(v);
+
+    for (int i = 0; i < answer.size(); i++) {
+        for (int j = 0; j < answer[i].size(); j++) {
+            cout << answer[i][j] << " ";
+        }
+        cout << endl;
+    }
+ 
+
+}
+
