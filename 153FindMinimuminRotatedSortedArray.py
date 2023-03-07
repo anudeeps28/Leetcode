1a1c901ad557f77e9d48ff30b76cee13d3a7ab2d
anudeeps28 <anudeepsharma28@gmail.com>
2023-03-07 15:30:03 -0500

    sorted + log n = binary click

---
+++ 153FindMinimuminRotatedSortedArray.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findMin(self, nums) -> int:
+        left = 0
+        right = len(nums)-1
+
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid  
+        return nums[left]
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    array = [3,4,5,1,2]
+    print(s.findMin(array))
