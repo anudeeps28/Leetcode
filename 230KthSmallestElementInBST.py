c58cde9226cdaaa9589e0e9edd895eacc6b0fdbc
anudeeps28 <anudeepsharma28@gmail.com>
2023-03-11 01:38:29 -0500

    recursive approach

--- 230KthSmallestElementInBST.py
+++ 230KthSmallestElementInBST.py
@@ -21,22 +21,22 @@ class Solution:
                 return curr.val
             curr = curr.right
 
-# Recursive approach
-def kthSmallest(self, root, k):
-    self.k = k
-    self.res = None
-    self.helper(root)
-    return self.res
+    # Recursive approach
+    def kthSmallest(self, root, k):
+        self.k = k
+        self.res = None
+        self.helper(root)
+        return self.res
 
-def helper(self, node):
-    if not node:
-        return
-    self.helper(node.left)
-    self.k -= 1
-    if self.k == 0:
-        self.res = node.val
-        return
-    self.helper(node.right)
+    def helper(self, node):
+        if not node:
+            return
+        self.helper(node.left)
+        self.k -= 1
+        if self.k == 0:
+            self.res = node.val
+            return
+        self.helper(node.right)
 
 
 if __name__ == "__main__":
