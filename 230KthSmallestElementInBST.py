89ba41bcc3ef4761996aa58fa0e349a11d7b7d08
anudeeps28 <anudeepsharma28@gmail.com>
2023-03-11 01:35:14 -0500

    iterative traversal

---
+++ 230KthSmallestElementInBST.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        stack = deque()        
+        curr = root
+        
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
+
+
+if __name__ == "__main__":
+    
+    s = Solution()
+
+    node2 = TreeNode(2)
+    node1 = TreeNode(1)
+    node4 = TreeNode(4)
+    node3 = TreeNode(3)
+
+    node1.right = node2
+    node3.left = node1
+    node3.right = node4
+
+    s.kthSmallest(node3, 1)
+
