f01235380267669b93ac6a6f9e7865e39a6c8dcd
anudeeps28 <anudeepsharma28@gmail.com>
2023-03-15 15:15:55 -0400

    dfs classic

---
+++ 133CloneGraph.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+
+            for neighbor in node.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            return copy
+            
+        return dfs(node) if node else None
+
+
+if __name__ == "__main__":
+    s = Solution()
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+
+    node1.neighbors = [node2, node4]
+    node2.neighbors = [node1, node3]
+    node3.neighbors = [node2, node4]
+    node4.neighbors = [node1, node3]
+
+    s.cloneGraph(node1)
