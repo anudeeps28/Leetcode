5bddaf07b1dc8efbd0983af1d3f4dc42fd1eb218
anudeeps28 <anudeepsharma28@gmail.com>
2023-03-21 21:03:57 -0400

    completing; recursion

--- 203RemoveLinkedListElements.py
+++ 203RemoveLinkedListElements.py
@@ -1,3 +1,11 @@
+from typing import List
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         dummyHead = ListNode(-1)
