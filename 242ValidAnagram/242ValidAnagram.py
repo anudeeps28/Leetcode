37b47b282772f28c7fbe4be0b803a14036755165
anudeeps28 <anudeepsharma28@gmail.com>
2025-01-10 14:41:12 -0500

    correction

--- 242ValidAnagram/242ValidAnagram.py
+++ 242ValidAnagram/242ValidAnagram.py
@@ -1,16 +1,16 @@
 def isAnagram(letter1, letter2):
     hashmap = {}
-    for letter in s:
-        if letter in hashmap:
-            hashmap[letter] += 1
+    for letter1 in s:
+        if letter1 in hashmap:
+            hashmap[letter1] += 1
         else:
-            hashmap[letter] = 1
+            hashmap[letter1] = 1
     
-    for letter in t:
-        if letter in hashmap:
-            hashmap[letter] -= 1
+    for letter2 in t:
+        if letter2 in hashmap:
+            hashmap[letter2] -= 1
         else:
-            hashmap[letter] = -1
+            hashmap[letter2] = -1
 
     for letter, frequency in hashmap.items():
         if frequency != 0:
